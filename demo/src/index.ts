5cfbe9bc58641378686e50b47618c4566892e400
pacry241 <phanduyman241@gmail.com>
2023-03-12 05:18:33 +0700

    up

---
+++ demo/src/index.ts
@@ -0,0 +1,12 @@
+// happy coding 👻
+console.log("hello world");
+
+let height: number;
+let width: number;
+
+width = 10;
+height = 20;
+
+let area: number = width * height;
+console.log(`Diện tích hình chữ nhật: ${area}`);
+
